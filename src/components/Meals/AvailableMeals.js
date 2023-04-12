b764bce04033550b2f13656f7a4a35a8fe8eb9e6
tewarimohit <tewarimohit955@gmail.com>
2023-04-12 22:11:15 +0530

    connected to database

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,46 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./AvailableMeals.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
-const availableMeals = DUMMY_MEALS.map((meal) => (
-  <MealItem
-    id={meal.id}
-    key={meal.id}
-    name={meal.name}
-    description={meal.description}
-    price={meal.price}
-  />
-));
+import Loader from "../UI/Loader";
 
 const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const fetchMeals = async () => {
+      setIsLoading(true);
+      const response = await fetch(
+        "https://custom-hook-a663f-default-rtdb.firebaseio.com/meals.json"
+      );
+      const data = await response.json();
+      const loadedData = [];
+      for (let key in data) {
+        loadedData.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+      setMeals(loadedData);
+      setIsLoading(false);
+    };
+
+    fetchMeals();
+  }, []);
+
+  const availableMeals = meals.map((meal) => (
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  if (isLoading) {
+    return (
+      <section className="loader">
+        <Loader />
+      </section>
+    );
+  }
+
   return (
     <section className="meals">
       <Card>
